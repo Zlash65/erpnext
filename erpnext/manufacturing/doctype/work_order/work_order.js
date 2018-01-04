8bb5756f8b30e80208cdb62b7d219a9241685df9
Zlash65 <zarrar65@gmail.com>
2018-01-04 16:56:38 +0530

    remove occurrences of Production Order

--- erpnext/manufacturing/doctype/work_order/work_order.js
+++ erpnext/manufacturing/doctype/work_order/work_order.js
@@ -95,7 +95,7 @@ frappe.ui.form.on("Work Order", {
 			erpnext.work_order.set_default_warehouse(frm);
 		}
 
-		// formatter for production order operation
+		// formatter for work order operation
 		frm.set_indicator_formatter('operation',
 			function(doc) { return (frm.doc.qty==doc.completed_qty) ? "green" : "orange" });
 	},
